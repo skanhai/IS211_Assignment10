3ce22171079757010bed11f32c28f9a4a5a79d08
skanhai <104988963+skanhai@users.noreply.github.com>
2024-04-01 23:47:26 -0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
+CREATE TABLE Artists (
+    artist_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Albums (
+    album_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id)
+);
+
+
+CREATE TABLE Songs (
+    song_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    album_id INT NOT NULL,
+    track_number INT NOT NULL,
+    duration_seconds INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
+);
 
